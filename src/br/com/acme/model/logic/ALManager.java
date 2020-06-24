b2e8081c8889d5d9a2ba3c98b58dee495830fc5e
BahNegao00 <gustavowo2010@hotmail.com>
2020-06-24 16:06:23 -0300

    xcvxcvxc

--- src/br/com/acme/model/logic/ALManager.java
+++ src/br/com/acme/model/logic/ALManager.java
@@ -2,10 +2,15 @@ package br.com.acme.model.logic;
 
 import br.com.acme.model.AcademicLibrary;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class ALManager {
+public class ALManager implements Serializable{
 
     private static AcademicLibrary library;
 
@@ -26,14 +31,33 @@ public class ALManager {
      * arquivo no disco rígido onde tenha sido armazenado o objeto 'library'.
      */
     public static void loadLibrary(File file) throws FileNotFoundException, IOException, ClassNotFoundException {
-
-    }
+        try{
+        FileInputStream fin = new FileInputStream("c:\\library.exe");
+        ObjectInputStream ois = new ObjectInputStream(fin);
+        library = (AcademicLibrary)ois.readObject();
+        ois.close();
+            System.out.println(library);
+        }catch(FileNotFoundException ex){
+            System.out.println("Não foi encontrado arquivo!!");
+        }
+        
+}
+        
 
     /**
      * Implemente o código deste método de forma que o objeto 'library' seja
      * persistido em um arquivo no disco rígido.
      */
     public static void persistLibrary(File file) throws FileNotFoundException, IOException {
+        try{
+        FileOutputStream fout = new FileOutputStream("c:\\library.exe");
+        ObjectOutputStream oos = new ObjectOutputStream(fout);
+        oos.writeObject(library);
+        oos.close();
+        System.out.println("👍");
+        }catch(IOException ex){
+            System.out.println();
+        }
 
     }
     
