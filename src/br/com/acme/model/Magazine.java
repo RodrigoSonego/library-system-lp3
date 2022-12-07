976e353c9f7bfbb9c18dbec0b3b8314c76071b66
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-12-07 20:11:38 -0300

    classe de Magazine

---
+++ src/br/com/acme/model/Magazine.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.acme.model;
+
+/**
+ *
+ * @author rodrigo.sonego
+ */
+public class Magazine extends Publication {
+    private String issue;
+
+    public Magazine(String title, short year, String author, String issue) {
+        super(title, year);
+        this.setAuthor(author);
+        this.issue = issue;
+    }
+    
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+    
+}
