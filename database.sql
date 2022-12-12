6a4ae2f20c3a2d15016e38cc96deb767f699f6de
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2022-12-12 20:55:06 -0300

    Modifica database

--- database.sql
+++ database.sql
@@ -11,12 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Banco de dados: `library_system`
 --
@@ -164,7 +158,8 @@ CREATE TABLE `magazine` (
   `title` varchar(60) NOT NULL,
   `year` int(10) NOT NULL,
   `author` varchar(50) NOT NULL,
-  `issue` varchar(55) NOT NULL
+  `doi` varchar(55) NOT NULL,
+  `topic` varchar(55) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `magazine`
